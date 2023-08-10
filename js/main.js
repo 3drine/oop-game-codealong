649c4d58f5167e547f9924f26638e9eb4a5a55f6
Sandrine Manel <sandrine.manelgmail.com>
2023-08-10 16:33:15 +0200

    added cat, rockets, background and random x axis for obstacles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 class Player { 
     constructor(){
-        console.log("inside the constructor")
         this.positionX = 50;
         this.positionY = 0;
-        this.width = 20; 
+        this.width = 10; 
         this.height = 20;
         this.domElement = null;
         this.createDomElement();
@@ -27,19 +26,22 @@ class Player {
 
 
     moveLeft(){
-        this.positionX--;
-        this.domElement.style.left = this.positionX+'vw';
+        if(this.positionX>0) {
+            this.positionX--;
+            this.domElement.style.left = this.positionX+'vw';
+        }
+        
     }
     moveRight(){
-        this.positionX++;
-        this.domElement.style.left = this.positionX+'vw';
+        if(this.positionX<100-this.width) {
+            this.positionX++;
+            this.domElement.style.left = this.positionX+'vw';
+        }
     }
-
 }
 
 const player = new Player(); 
 document.addEventListener('keydown', (event)=> {
-    console.log("user pressed sth"+event.key);
     if(event.key === "ArrowLeft") {
         player.moveLeft();
 
@@ -49,6 +51,55 @@ document.addEventListener('keydown', (event)=> {
 
     }
 })
+
+
+class Obstacle {
+    constructor(){
+        this.positionX = Math.random() * (80 - 0) + 0;
+        this.positionY = 100;
+        this.width = 5; 
+        this.height = 15;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    moveDown(){
+            this.positionY--;
+            this.domElement.style.bottom = this.positionY + "vh"
+    }
+    createDomElement(){
+        this.domElement = document.createElement('div');
+        this.domElement.className = 'obstacle';
+        this.domElement.style.width = this.width+ "vw";
+        this.domElement.style.height = this.height+ "vh";
+        this.domElement.style.left = this.positionX+ "vw";
+        this.domElement.style.bottom = this.positionY+ "vh";
+
+        //this.domElement.setAttribute('id','player')
+        const parentElm = document.getElementById('board');
+        parentElm.appendChild(this.domElement);
+    }
+
+}
+
+
+
+const obstaclesArr = [];
+
+//create obstacles
+
+setInterval(() => {
+    let newObstacle = new Obstacle;
+    //newObstacle.moveDown();
+    obstaclesArr.push(newObstacle)
+}, 10000)
+
+// move all obstacles 
+setInterval(() => {
+    obstaclesArr.forEach((element) => {
+        element.moveDown();
+    })
+}, 20);
 
 
 
